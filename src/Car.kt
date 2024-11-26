83af9928661ed92d76fc0583abde1d42ea20c8b7
Gustavo Lizardo <gustavolizardo123oliveira@gmail.com>
2024-11-26 18:00:35 -0300

    #

---
+++ src/Car.kt
@@ -0,0 +1,18 @@
+﻿class Car {
+
+    constructor(cor: String?, marca: String?){
+        this.cor = cor
+        this.marca = marca
+    }
+
+    private var cor: String? = null
+    private var marca: String? = null
+
+    fun Andar(){
+        println("Carro $marca da cor $cor Andando...")
+    }
+
+    fun Parar(){
+        println("Carro $marca da cor $cor Parando...")
+    }
+}
---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    var car = Car("Fiat", "Amarelo")
+    car.Andar()
+    car.Parar()
+
+
+    var car2 = Car("Volks", "Rosa")
+    car2.Andar()
+    car2.Parar()
+
+    var car3 = Car("Chevrolet", "Azul")
+    car3.Andar()
+    car3.Parar()
+}
+async fun
